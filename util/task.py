7d589f1d977ccbc56d216281e97a3f0996de5ecf
Jan Horacek <me@apophis.cz>
2019-05-11 14:04:30 +0200

    Outsource some code to .py files.

---
+++ util/task.py
@@ -0,0 +1,17 @@
+from sqlalchemy import func, distinct, or_, desc
+
+from db import session
+import model
+import util
+
+
+def points_per_module_all_years():
+    return session.query(
+        model.Wave.year.label('year_id'),
+        model.Evaluation.user.label('user'),
+        func.max(model.Evaluation.points).label('points'),
+    ).\
+        join(model.Evaluation.r_module).join(model.Module.r_task).\
+        join(model.Task.r_wave).\
+        group_by(model.Wave.year, model.Evaluation.user,
+                 model.Evaluation.module)
