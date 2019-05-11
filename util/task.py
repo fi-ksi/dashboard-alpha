bf4d8d8ed3aca1b47ff50b325eb1050e3ebd194b
Jan Horacek <me@apophis.cz>
2019-05-11 16:07:46 +0200

    Add points histogram.

--- util/task.py
+++ util/task.py
@@ -15,3 +15,39 @@ def points_per_module_all_years():
         join(model.Task.r_wave).\
         group_by(model.Wave.year, model.Evaluation.user,
                  model.Evaluation.module)
+
+
+def _max_points_per_wave(bonus=False):
+    points_per_task = session.query(
+        model.Module.task.label('id'),
+        func.sum(model.Module.max_points).label('points')
+    )
+    if not bonus:
+        points_per_task = points_per_task.filter(model.Module.bonus == False)
+    points_per_task = points_per_task.group_by(model.Module.task).subquery()
+
+    return session.query(model.Wave.id.label('id'),
+                         func.sum(points_per_task.c.points).label('points'),
+                         func.count(model.Task.id).label('tasks_count')).\
+        outerjoin(model.Task, model.Task.wave == model.Wave.id).\
+        outerjoin(points_per_task, points_per_task.c.id == model.Task.id).\
+        group_by(model.Wave)
+
+
+def max_points_per_year(bonus=False):
+    """{year_id: (max_points, tasks_count)}"""
+    points_per_wave = _max_points_per_wave(bonus).subquery()
+    points_per_year = session.query(
+        model.Year.id.label('id'),
+        func.sum(points_per_wave.c.points).label('points'),
+        func.sum(points_per_wave.c.tasks_count).label('tasks_count')
+    ).\
+        outerjoin(model.Wave, model.Wave.year == model.Year.id).\
+        outerjoin(points_per_wave, points_per_wave.c.id == model.Wave.id).\
+        group_by(model.Year).all()
+
+    return {
+        year.id: (year.points if year.points else 0.0,
+                  int(year.tasks_count) if year.tasks_count else 0)
+        for year in points_per_year
+    }
