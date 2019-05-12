cf44a2bc49bcd2170016a40d574a7209d5f01031
Jan Horacek <me@apophis.cz>
2019-05-12 20:23:44 +0200

    Fix path in deploy.sh.

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,5 +2,5 @@
 
 echo -e "Content-type: text/html\n"
 
-OUTPUT+=$(cd ../.. && ./kleobis-deploy.sh 2>&1)$'\n\n'
+OUTPUT+=$(cd .. && ./kleobis-deploy.sh 2>&1)$'\n\n'
 echo "$OUTPUT" | mail "me@apophis.cz" -s "[ksi-monitoring] Deploy status"
